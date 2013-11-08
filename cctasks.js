a3e5b1915ceddb3d59b1d9ef81d592932f4432f8
Mark Stacy <mbstacy@gmail.com>
2013-11-08 12:50:25 -0600

    Javascript updates with code reformat within pyCharm

--- cctasks.js
+++ cctasks.js
@@ -4,28 +4,28 @@
 // Parameters can be specified in [params] list object, or a special list of 
 // jQuery selectors can be provided to grab the current values of these form elements at run-time.
 /*
-taskdesc = { 
-    "taskname":   'cybercomq.static.tasks.modiscountry',
-    "taskq":      'static',
-    "params":     ['MOD09A1_ndvi','MX','2010-10-10','2010-11-1'],   // Fixed params 
-    "uiparams":   ['#product','#country','#start_date','#end_date'],// UI Selected
-    "status":     '#status',
-    "spinner":    '#spinner',
-    "pollinterval": 2000,
-}
+ taskdesc = {
+ "taskname":   'cybercomq.static.tasks.modiscountry',
+ "taskq":      'static',
+ "params":     ['MOD09A1_ndvi','MX','2010-10-10','2010-11-1'],   // Fixed params
+ "uiparams":   ['#product','#country','#start_date','#end_date'],// UI Selected
+ "status":     '#status',
+ "spinner":    '#spinner',
+ "pollinterval": 2000,
+ }
 
-*/
+ */
 // Called by call task to poll queue status of task based on task_id
 var QUEUE_SUBMIT = 'http://test.oklahomawatersurvey.org/queue/run/';
-var QUEUE_POLL =  'http://test.oklahomawatersurvey.org/queue/task/';
+var QUEUE_POLL = 'http://test.oklahomawatersurvey.org/queue/task/';
 
-function test_auth_tkt() {
+function test_auth_tkt () {
     $("#auth_dialog").hide();
-    if ($.cookie('auth_tkt') ) {
+    if ($.cookie('auth_tkt')) {
         $('#auth_message').html("you're logged in")
     }
     else {
-        $("#auth_dialog").dialog( { height:200, modal: true} );
+        $("#auth_dialog").dialog({ height: 200, modal: true});
         $("#auth_dialog").dialog("open");
         $('#auth_message').html('Please <a href="http://test.cybercommons.org/accounts/login/">login</a> to track your tasks via the cybercommons').addClass('label warning');
     }
@@ -33,22 +33,21 @@ function test_auth_tkt() {
 }
 
 
-
-function poll_status(args) {
+function poll_status (args) {
     $.getJSON(args.host + args.task_id + '?callback=?', function (data) {
         if (data.status == "PENDING") {
             options.onPending(args);
         } else if (data.status == "FAILURE") {
             options.onFailure(data);
-        } else if (data.status == "Error"){
-            options.onFailure(data); 
+        } else if (data.status == "Error") {
+            options.onFailure(data);
         } else if (data.status == "SUCCESS") {
             options.onSuccess(data);
         }
     });
 }
 
-function calltask(taskdesc) {
+function calltask (taskdesc) {
     defaults = {
         "service_host": QUEUE_SUBMIT,
         "poll_target": QUEUE_POLL,
@@ -73,7 +72,7 @@ function calltask(taskdesc) {
         "onSuccess": function (data) {
             $(options.status).show();
             $(options.status).removeClass('label label-success label-warning label-important').addClass('label label-success');
-            $(options.status).html('<a href="'+ data.tombstone[0].result + '">Download</a>');
+            $(options.status).html('<a href="' + data.tombstone[0].result + '">Download</a>');
             $(options.spinner).hide();
             simpleCart.empty();
         },
@@ -91,10 +90,10 @@ function calltask(taskdesc) {
             taskparams = taskparams.concat('/' + $(options.uiparams[item]).val());
         }
     }
-    var kwargs ="?";
-    if(options.kwargs) {
-        for (var key in options.kwargs){
-            kwargs=kwargs + key + '=' + options.kwargs[key] + '&';
+    var kwargs = "?";
+    if (options.kwargs) {
+        for (var key in options.kwargs) {
+            kwargs = kwargs + key + '=' + options.kwargs[key] + '&';
         }
     }
     var taskcall = "";
